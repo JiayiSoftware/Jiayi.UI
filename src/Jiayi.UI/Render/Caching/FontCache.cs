90b4cf642b465c8d7b3f174691d5dbbd19577d2e
phase <64127681+phasephasephase@users.noreply.github.com>
2025-01-09 13:52:45 -0700

    start font stuff

---
+++ src/Jiayi.UI/Render/Caching/FontCache.cs
@@ -0,0 +1,12 @@
+﻿using Vortice.DirectWrite;
+
+namespace Jiayi.UI.Render.Caching;
+
+public class FontCache
+{
+	// pretty much everything in DWrite is device independent
+	private IDWriteFactory5 _writeFactory;
+	private IDWriteFontCollection1 _fontCollection;
+	private Dictionary<int, IDWriteTextFormat> _textFormats;
+	private Dictionary<int, IDWriteTextLayout> _textLayouts;
+}
